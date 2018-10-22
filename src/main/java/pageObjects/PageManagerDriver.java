4ecdbb22f97d9e61f5d46f1fa95d0590262e804b
Bui Quang Toan <bqt1089@gmail.com>
2018-10-22 21:33:32 +0700

    test commit

--- src/main/java/pageObjects/PageManagerDriver.java
+++ src/main/java/pageObjects/PageManagerDriver.java
@@ -6,25 +6,26 @@ public class PageManagerDriver {
 	private static HomePageObject homePageObject;
 	private static LoginPageObject loginPageObject;
 	private static RegisterPageObject registerPageObject;
-	
+
 	public static HomePageObject getHomePageDriver(WebDriver driver) {
 		if (homePageObject == null) {
-			  new HomePageObject(driver);
-		} 
+			new HomePageObject(driver);
+		}
 		return homePageObject;
 	}
+
 	public static LoginPageObject getLoginPageDriver(WebDriver driver) {
 		if (loginPageObject == null) {
 			new LoginPageObject(driver);
-		} 
+		}
 		return loginPageObject;
 	}
+
 	public static RegisterPageObject getRegisterPageDriver(WebDriver driver) {
 		if (registerPageObject == null) {
 			new RegisterPageObject(driver);
-		} 
+		}
 		return registerPageObject;
 	}
-	
 
 }
